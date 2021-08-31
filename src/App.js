a344dd4f37e3679f8be985a7456b6317721b57a2
Novik Aliaksandr <navaliny1989@gmail.com>
2021-08-31 22:26:51 +0300

    lesson 60 with mistake

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, {
-  Component
-} from 'react';
-import './App.css';
-import Header from './Components/Header/Header';
-import Navbar from './Components/Navbar/Navbar';
-import DialoguesContainer from './Components/Dialogues/DialoguesContainer';
-import News from './Components/News/News';
-import Setting from './Components/Setting/Setting';
-import Music from './Components/Music/Music';
-import UsersContainer from './Components/Users/UsersContainer';
-
-import { Route } from 'react-router-dom'
-import ProfileContainer from './Components/Profile/ProfileContainer';
-
-
-const App = () => {
- 
-
- 
-  return (
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/Dialogues" render={()=> <DialoguesContainer/>} />
-          <Route path="/Profile" render={()=> <ProfileContainer/>} />
-          <Route path="/Users" render={()=> <UsersContainer/>} />
-          <Route path="/News" render={()=> <News/>} />
-          <Route path="/Setting" render={()=> <Setting/>} />
-          <Route path="/Music" render={()=> <Music/>} />
-
-          
+    Component
+  } from 'react';
+  import './App.css';
+  import Header from './Components/Header/Header';
+  import Navbar from './Components/Navbar/Navbar';
+  import DialoguesContainer from './Components/Dialogues/DialoguesContainer';
+  import News from './Components/News/News';
+  import Setting from './Components/Setting/Setting';
+  import Music from './Components/Music/Music';
+  import UsersContainer from './Components/Users/UsersContainer';
+  
+  import { Route } from 'react-router-dom';
+  import ProfileContainer from './Components/Profile/ProfileContainer';
+  
+  
+  const App = () => {
+   
+  
+   
+    return (
+        <div className="app-wrapper">
+          <Header />
+          <Navbar />
+          <div className="app-wrapper-content">
+            <Route path="/Dialogues" render={()=> <DialoguesContainer/>} />
+            <Route path='/profile/:userId' render={()=> <ProfileContainer/>} />
+            <Route path="/Users" render={()=> <UsersContainer/>} />
+            <Route path="/News" render={()=> <News/>} />
+            <Route path="/Setting" render={()=> <Setting/>} />
+            <Route path="/Music" render={()=> <Music/>} />
+  
+            
+          </div>
         </div>
-      </div>
-  );
-}
+    );
+  }
+  
+  export default App;
 
-export default App;
+  
+
