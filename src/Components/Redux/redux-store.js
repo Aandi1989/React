6a39f0366a4712b695f17f5a17008ecea9003554
Novik Aliaksandr <navaliny1989@gmail.com>
2021-12-02 15:49:00 +0300

    75lesson

--- src/Components/Redux/redux-store.js
+++ src/Components/Redux/redux-store.js
@@ -3,15 +3,17 @@ import dialoguesReducer from "./dialogues-reducer";
 import usersReducer from "./users-reducer";
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import authReducer from "./auth-reducer";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 let reducers=combineReducers({
     profilePage: profileReducer,
     dialoguesPage: dialoguesReducer,
     usersPage:usersReducer,
-    auth:authReducer
+    auth:authReducer,
+    form:formReducer
 });
 
 let store= createStore(reducers, applyMiddleware(thunkMiddleware));
-
+window.store=store
 export default store;
